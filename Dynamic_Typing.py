2fb24cdc8955a278ad437ce74c64e5de6093f4b2
riteshth1 <riteshbill14@gmail.com>
2024-09-25 23:46:44 +0545

    Quiz app with GUI

---
+++ Dynamic_Typing.py
@@ -0,0 +1,23 @@
+# age: int
+# name: str
+# height: float
+# is_human: bool
+
+# Type Hints
+
+# def police_check(age: int) -> bool:
+#     if age > 18:
+#         can_drive = True
+#     else:
+#         can_drive = False
+#     return can_drive
+#
+# if police_check(12):
+#     print("You may pass.")
+# else:
+#     print("Pay a fine.")
+
+# def greet(name:str) -> str:
+#     return "Hello " + name
+#
+# print(greet("ritesh"))
--- ui.py
+++ ui.py
@@ -1,13 +1,13 @@
 from tkinter import *
-# from turtledemo.nim import randomrow
-
-# from charset_normalizer.utils import iana_name
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,19 +16,45 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Question" ,fill=THEME_COLOR)
-        # Creating screens
+        self.question_text = self.canvas.create_text(150, 125,width=280, text="Question" ,fill=THEME_COLOR, font=("Arial",15, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
 
         # Creating Buttons
         right_img = PhotoImage(file="images/true.png")
-        r_btn = Button(image=right_img, bg=THEME_COLOR, highlightthickness=0)
-        r_btn.grid(row=3, column=1)
+        self.r_btn = Button(image=right_img, bg=THEME_COLOR, highlightthickness=0, command= self.true_pressed)
+        self.r_btn.grid(row=2, column=0)
 
         false_img = PhotoImage(file='images/false.png')
-        false_btn = Button(image=false_img, bg=THEME_COLOR, highlightthickness=0)
-        false_btn.grid(row=3 ,column=2)
+        self.false_btn = Button(image=false_img, bg=THEME_COLOR, highlightthickness=0, command= self.false_pressed)
+        self.false_btn.grid(row=2 ,column=1)
+
+        self.get_new_question()
 
         self.window.mainloop()
 
-Demo = QuizInterface()
+    def get_new_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score:{self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="Yov've reached the end of the day")
+            self.true_pressed.config(state="disabled")
+            self.false_pressed.config(state="disabled")
+
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right= self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_new_question())
